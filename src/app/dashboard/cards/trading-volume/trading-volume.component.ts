bdb29536b0fd7ec53b8eefdf2bfc0c8d480718ff
GonzoHacker <gonzo@bitcap.com>
2018-03-06 20:03:27 -0600

    trading volume

--- src/app/dashboard/cards/trading-volume/trading-volume.component.ts
+++ src/app/dashboard/cards/trading-volume/trading-volume.component.ts
@@ -1,15 +1,123 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-trading-volume',
   templateUrl: './trading-volume.component.html',
   styleUrls: ['./trading-volume.component.css']
 })
-export class TradingVolumeComponent implements OnInit {
+export class TradingVolumeComponent implements AfterViewInit {
 
-  constructor() { }
+  chartColor = '#FFFFFF';
 
-  ngOnInit() {
+  @ViewChild('tradingVolumeChart') chart;
+
+  constructor() {}
+
+  ngAfterViewInit() {
+    this.loadChart();
   }
 
+  loadChart() {
+    const cardStatsMiniLineColor = '#fff';
+    const cardStatsMiniDotColor = '#fff';
+
+    const ctx = this.chart.nativeElement.getContext('2d');
+
+    const gradientStroke = ctx.createLinearGradient(500, 0, 100, 0);
+    gradientStroke.addColorStop(0, '#80b6f4');
+    gradientStroke.addColorStop(1, this.chartColor);
+
+    const gradientFill = ctx.createLinearGradient(0, 170, 0, 50);
+    gradientFill.addColorStop(0, 'rgba(128, 182, 244, 0)');
+    gradientFill.addColorStop(1, 'rgba(249, 99, 59, 0.40)');
+
+    const gradientChartOptionsConfiguration = {
+      maintainAspectRatio: false,
+      legend: {
+        display: false
+      },
+      tooltips: {
+        bodySpacing: 4,
+        mode: 'nearest',
+        intersect: 0,
+        position: 'nearest',
+        xPadding: 10,
+        yPadding: 10,
+        caretPadding: 10
+      },
+      responsive: true,
+      scales: {
+        yAxes: [
+          {
+            display: 0,
+            ticks: {
+              display: false
+            },
+            gridLines: {
+              zeroLineColor: 'transparent',
+              drawTicks: false,
+              display: false,
+              drawBorder: false
+            }
+          }
+        ],
+        xAxes: [
+          {
+            display: 0,
+            ticks: {
+              display: false
+            },
+            gridLines: {
+              zeroLineColor: 'transparent',
+              drawTicks: false,
+              display: false,
+              drawBorder: false
+            }
+          }
+        ]
+      },
+      layout: {
+        padding: { left: 0, right: 0, top: 15, bottom: 15 }
+      }
+    };
+
+    const myChart = new Chart(ctx, {
+      type: 'line',
+      responsive: true,
+      data: {
+        labels: [
+          'Jan',
+          'Feb',
+          'Mar',
+          'Apr',
+          'May',
+          'Jun',
+          'Jul',
+          'Aug',
+          'Sep',
+          'Oct',
+          'Nov',
+          'Dec'
+        ],
+        datasets: [
+          {
+            label: 'Trading Volume',
+            borderColor: '#f96332',
+            pointBorderColor: '#FFF',
+            pointBackgroundColor: '#f96332',
+            pointBorderWidth: 2,
+            pointHoverRadius: 4,
+            pointHoverBorderWidth: 1,
+            pointRadius: 4,
+            fill: true,
+            backgroundColor: gradientFill,
+            borderWidth: 2,
+            data: [542, 480, 430, 550, 530, 453, 380, 434, 568, 610, 700, 630]
+          }
+        ]
+      },
+      options: gradientChartOptionsConfiguration
+    });
+  }
 }
